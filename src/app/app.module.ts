a8dd5e7ca527a0304a9a5a1a7279a8c93b1eb3a7
Gabriel Henrique Gomes Firmino <gabriel.henrique.gf01@gmail.com>
2021-06-21 21:28:10 -0300

    ngFood
    Projeto de um sistemas de pedidos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from "@angular/common/http";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatBadgeModule } from "@angular/material/badge";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PedidoComponent } from './pedido/pedido.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidoComponent,
+    CardapioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
